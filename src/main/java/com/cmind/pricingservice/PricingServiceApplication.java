77d435429e95d972d1f869ef786cdb1b2d277169
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-31 18:29:31 +0530

    Update PricingServiceApplication.java

--- src/main/java/com/cmind/pricingservice/PricingServiceApplication.java
+++ src/main/java/com/cmind/pricingservice/PricingServiceApplication.java
@@ -1,1 +1,14 @@
+package com.cmind.pricingservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class PricingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
+}
