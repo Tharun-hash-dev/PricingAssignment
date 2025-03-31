db0e15c1dcb32384ca5bc49f8150de560595e5ce
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-31 18:22:05 +0530

    Update Price.java

--- src/main/java/com/cmind/pricingservice/model/Price.java
+++ src/main/java/com/cmind/pricingservice/model/Price.java
@@ -1,1 +1,27 @@
+package com.cmind.pricingservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Entity
+@Data
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private String subtype;
+    private String currency;
+    private BigDecimal amount;
+    private ZonedDateTime validFrom;
+    private ZonedDateTime validTo;
+    private boolean overlapped;
+
+}
