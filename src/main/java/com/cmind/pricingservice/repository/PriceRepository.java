c585e213d631b1c35ff5c5fb32091e024a23cf09
Tharun-hash-dev <bhukyatharun67@gmail.com>
2025-03-31 18:24:21 +0530

    Update PriceRepository.java

--- src/main/java/com/cmind/pricingservice/repository/PriceRepository.java
+++ src/main/java/com/cmind/pricingservice/repository/PriceRepository.java
@@ -1,1 +1,13 @@
+package com.cmind.pricingservice.repository;
 
+import com.cmind.pricingservice.model.Article;
+import com.cmind.pricingservice.model.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PriceRepository extends JpaRepository<Price, Long> {
+    List<Price> findByArticleIdAndStoreId(String articleId, String storeId);
+    Article findArticleByArticleIdAndStoreId(String articleId, String storeId);
+}
